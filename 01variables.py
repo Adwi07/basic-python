015a71799e163020d4be62120f55b5d718bf737b
Adwitiya Gupta <guptadwi07@gmail.com>
2026-08-28 22:03:09 +0530

    minor changes

--- 01variables.py
+++ 01variables.py
@@ -1,20 +1,37 @@
+#basic data types including string, int, float and bool(boolean) 
+
+current_learning_topic = "Learning variables and how to name clean varables"
+print(current_learning_topic)
+print(type(current_learning_topic))
+
+
+#To display name,age,city college etc.
+
 my_name = "Adwitiya Gupta"
 print(my_name)
+print(type(my_name)) #to print the data type it actually is
 
 my_age = 22
 print(my_age)
+print(type(my_age))
 
 my_city = "Bilaspur"
 print(my_city)
+print(type(my_city))
 
 my_college = "Shri shankaracharya technical campus"
 print(my_college)
+print(type(my_college))
 
 my_experience = 0
 print(my_experience)
+print(type(my_experience))
 
 Liking_for_python = bool(int(input("Do you like python? type 1 for yes/true or 0 for no/false ")))
 print(Liking_for_python)
+print(type(Liking_for_python))
 
 expectedSalary = float(input("What is your expected salary after learning python? "))
 print(expectedSalary)
+print(type(expectedSalary))
+
