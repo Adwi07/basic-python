3a7bd5efc479c16c27edf8b473faeba6ae0ba018
Adwitiya Gupta <guptadwi07@gmail.com>
2026-08-28 20:31:28 +0530

    Add files via upload

---
+++ 01variables.py
@@ -0,0 +1,20 @@
+my_name = "Adwitiya Gupta"
+print(my_name)
+
+my_age = 22
+print(my_age)
+
+my_city = "Bilaspur"
+print(my_city)
+
+my_college = "Shri shankaracharya technical campus"
+print(my_college)
+
+my_experience = 0
+print(my_experience)
+
+Liking_for_python = bool(int(input("Do you like python? type 1 for yes/true or 0 for no/false ")))
+print(Liking_for_python)
+
+expectedSalary = float(input("What is your expected salary after learning python? "))
+print(expectedSalary)
